0fba95ec2d1616454d78a9f1fd9bb0f14af209ad
randolphu <randolphu@1b988373-acfa-0310-9a85-c19025f466bc>
2005-08-10 04:42:40 +0000

    more enhancements in apoll

--- trunk/include/pyasynchio/py_apoll.hpp
+++ trunk/include/pyasynchio/py_apoll.hpp
@@ -11,12 +11,40 @@
 
 namespace pyasynchio {
 
+template<typename CPP_TYPE, typename PYTHON_TYPE_OBJ_TYPE>
+CPP_TYPE* py_convert(PyObject *what, PYTHON_TYPE_OBJ_TYPE *type_obj)
+{
+	if (!PyObject_IsInstance(what, reinterpret_cast<PyObject*>(type_obj))) {
+		PyErr_Format(PyExc_TypeError, "not a %s object", type_obj->tp_name);
+		return NULL;
+	}
+	return reinterpret_cast<CPP_TYPE*>(what);
+}
+
 class PYASYNCHIO_LINK_DECL Py_apoll : public PyObject
 {
+public:
+	static const unsigned int addr_size = sizeof(sockaddr_in) + sizeof(sockaddr);
+	struct APOLL_OVERLAPPED : OVERLAPPED
+	{
+		APOLL_OVERLAPPED() 
+		{
+			Internal = InternalHigh = Offset = OffsetHigh = 0;
+			hEvent = 0;
+		}
+		virtual ~APOLL_OVERLAPPED() {}
+
+		static const unsigned int addr_buf_size = 2 * addr_size;
+
+		unsigned char addr_buf[addr_buf_size];
+	};
+
     Py_apoll();
     ~Py_apoll();
 
-    void accept(::PySocketSockObject *so, ::PyObject *act);
+    void accept(::PySocketSockObject *listen_socket
+		, ::PySocketSockObject *accept_socket
+		, ::PyObject *act);
     void connect(::PySocketSockObject *so, ::PyObject *addro, ::PyObject *acto);
     void send(::PySocketSockObject *so, ::PyObject *datao, unsigned long flags
         , ::PyObject *acto);
@@ -29,6 +57,10 @@ class PYASYNCHIO_LINK_DECL Py_apoll : public PyObject
 
 	::PyObject* poll(unsigned long ms);
 
+	static int init_func(PyObject *self, PyObject *args, PyObject *kwds);
+	static PyObject* accept_meth(Py_apoll *self, ::PyObject *args);
+
+private:
     HANDLE iocp_handle_;
 };
 
--- trunk/src/cplusplus/pyasynchio/py_apoll.cpp
+++ trunk/src/cplusplus/pyasynchio/py_apoll.cpp
@@ -1,4 +1,6 @@
 #include <pyasynchio/py_apoll.hpp>
+#include <mswsock.h>
+#include <new>
 
 namespace pyasynchio {
 
@@ -9,12 +11,55 @@ Py_apoll::Py_apoll()
 		, NULL // existing completion port
 		, NULL // completion key
 		, 0 // number of concurrent threads
-		);
+	);
+	if (NULL == iocp_handle_) {
+		PyErr_SetString(PyExc_RuntimeError, "::CreateIoCompletionPort failed");
+	}
 }
 
 Py_apoll::~Py_apoll()
 {
 	::CloseHandle(iocp_handle_);
+}
+
+void Py_apoll::accept(::PySocketSockObject *listen_sock
+					  , ::PySocketSockObject *accept_sock
+					  , ::PyObject *act)
+{
+	HANDLE iocp_ret = ::CreateIoCompletionPort(
+		reinterpret_cast<HANDLE>(listen_sock->sock_fd)
+		, iocp_handle_
+		, NULL
+		, 0);
+	if (iocp_handle_ != iocp_ret) {
+		::PyErr_Format(PyExc_RuntimeError
+			, "Failed to associate I/O completion port with socket handle %d"
+			, GetLastError());
+		return;
+	}
+
+	
+	DWORD num_bytes_rcvd = 0;
+	APOLL_OVERLAPPED *ao = new APOLL_OVERLAPPED();
+	BOOL r = ::AcceptEx(listen_sock->sock_fd
+		, accept_sock->sock_fd
+		, &ao->addr_buf[0]
+		, 0
+		, addr_size
+		, addr_size
+		, &num_bytes_rcvd
+		, ao);
+	if (TRUE == r) {
+		::PyErr_SetString(PyExc_RuntimeError, "operation completed synchronously");
+		return;
+	}
+	else {
+		if (WSAGetLastError() != ERROR_IO_PENDING) {
+			::PyErr_SetString(PyExc_RuntimeError, "something wrong happened");
+			return;
+		}
+		::PyErr_SetString(PyExc_RuntimeError, "operation completed asynchronously");
+	}
 }
 
 static PyTypeObject apoll_Type = {
@@ -42,13 +87,61 @@ static PyTypeObject apoll_Type = {
 	"Apoll objects",           /* tp_doc */
 };
 
+static PyMethodDef apoll_methods[] = {
+	{ "accept"
+		, reinterpret_cast<PyCFunction>(&Py_apoll::accept_meth)
+		, METH_VARARGS
+		, "start asynchronous accept operation"
+	}
+	, {NULL} /* Sentinel */
+};
+
+int Py_apoll::init_func(PyObject *self, PyObject *args, PyObject *kwds)
+{
+	char* keywords[] = { NULL };
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, ":apoll", keywords)) {
+		return NULL;
+	}
+	new (self) pyasynchio::Py_apoll;
+	return TRUE;
+}
+
+PyObject* Py_apoll::accept_meth(Py_apoll *self, ::PyObject *args)
+{
+	PyObject *listen_sock_raw, *accept_sock_raw, *act;
+	if (!PyArg_ParseTuple(args, "OOO:accept", &listen_sock_raw, &accept_sock_raw, &act)) {
+		return NULL;
+	}
+
+	PySocketSockObject *listen_sock = py_convert<PySocketSockObject>(listen_sock_raw
+		, PySocketModule.Sock_Type);
+	if (NULL == listen_sock) {
+		return NULL;
+	}
+
+	PySocketSockObject *accept_sock = py_convert<PySocketSockObject>(accept_sock_raw
+		, PySocketModule.Sock_Type);
+	if (NULL == accept_sock) {
+		return NULL;
+	}
+
+	self->accept(listen_sock, accept_sock, act);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
 static PyMethodDef pyasynchio_methods[] = {
 	{NULL}	/* Sentinel */
 };
 
 void initpyasynchio()
 {
+	::PySocketModule_ImportModuleAndAPI();
+
+	apoll_Type.tp_methods = apoll_methods;
 	apoll_Type.tp_new = PyType_GenericNew;
+	apoll_Type.tp_init = &Py_apoll::init_func;
 	if (PyType_Ready(&apoll_Type) < 0) {
 		return;
 	}
